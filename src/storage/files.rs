835eac2005b9eb384c617a93b86b7430e836dfeb
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2022-05-17 16:54:25 +0200

    FileKeeper: use &self instead of &mut self
    Both on unix and windows files can be read and written using
    immutable references. This allows to use Rc<FileKeeper> in the
    future.

--- src/storage/files.rs
+++ src/storage/files.rs
@@ -1,6 +1,5 @@
 use crate::storage::Error;
 use std::collections::BTreeMap;
-use std::io::SeekFrom;
 use std::path::PathBuf;
 use std::{fs, io};
 
@@ -30,14 +29,14 @@ impl FileKeeper {
         Ok(FileKeeper { files: filemap })
     }
 
-    pub fn write_block(&mut self, global_offset: usize, block: Vec<u8>) -> Result<(), Error> {
-        write_block_to(&mut self.files, global_offset, &block)?;
+    pub fn write_block(&self, global_offset: usize, block: Vec<u8>) -> Result<(), Error> {
+        write_block_to(&self.files, global_offset, &block)?;
         Ok(())
     }
 
-    pub fn read_block(&mut self, global_offset: usize, length: usize) -> Result<Vec<u8>, Error> {
+    pub fn read_block(&self, global_offset: usize, length: usize) -> Result<Vec<u8>, Error> {
         let mut dest = vec![0u8; length];
-        read_block_from(&mut self.files, global_offset, &mut dest)?;
+        read_block_from(&self.files, global_offset, &mut dest)?;
         Ok(dest)
     }
 }
@@ -48,20 +47,80 @@ impl Drop for FileKeeper {
     }
 }
 
+trait RandomAccessReadWrite {
+    fn read_at_offset(&self, dest: &mut [u8], offset: u64) -> io::Result<usize>;
+    fn write_at_offset(&self, src: &[u8], offset: u64) -> io::Result<usize>;
+
+    fn read_all_at_offset(&self, mut dest: &mut [u8], mut offset: u64) -> io::Result<()> {
+        while !dest.is_empty() {
+            let bytes_read = self.read_at_offset(dest, offset)?;
+            if bytes_read == 0 {
+                return Err(io::Error::new(
+                    io::ErrorKind::UnexpectedEof,
+                    "failed to fill whole buffer",
+                ));
+            }
+            dest = &mut dest[bytes_read..];
+            offset += bytes_read as u64;
+        }
+        Ok(())
+    }
+    fn write_all_at_offset(&self, mut src: &[u8], mut offset: u64) -> io::Result<()> {
+        while !src.is_empty() {
+            let bytes_written = self.write_at_offset(src, offset)?;
+            if bytes_written == 0 {
+                return Err(io::Error::new(
+                    io::ErrorKind::WriteZero,
+                    "failed to write whole buffer",
+                ));
+            }
+            src = &src[bytes_written..];
+            offset += bytes_written as u64;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(target_family = "unix")]
+impl RandomAccessReadWrite for fs::File {
+    fn read_at_offset(&self, dest: &mut [u8], offset: u64) -> io::Result<usize> {
+        use std::os::unix::prelude::*;
+        self.read_at(dest, offset)
+    }
+
+    fn write_at_offset(&self, src: &[u8], offset: u64) -> io::Result<usize> {
+        use std::os::unix::prelude::*;
+        self.write_at(src, offset)
+    }
+}
+
+#[cfg(target_family = "windows")]
+impl RandomAccessReadWrite for fs::File {
+    fn read_at_offset(&self, dest: &mut [u8], offset: u64) -> io::Result<usize> {
+        use std::os::windows::prelude::*;
+        self.seek_read(dest, offset)
+    }
+
+    fn write_at_offset(&self, src: &[u8], offset: u64) -> io::Result<usize> {
+        use std::os::windows::prelude::*;
+        self.seek_write(src, offset)
+    }
+}
+
 fn find_file_and_offset<F>(
-    all_files: &mut BTreeMap<usize, F>,
+    all_files: &BTreeMap<usize, F>,
     global_offset: usize,
-) -> Result<(usize, &mut F, usize), Error> {
+) -> Result<(usize, &F, usize), Error> {
     let next_start_offset = {
         let (offset, _) = all_files
-            .range_mut(global_offset + 1..)
+            .range(global_offset + 1..)
             .next()
             .ok_or(Error::InvalidLocation)?;
         *offset
     };
     let (start_offset, file) = {
         let (offset, file) = all_files
-            .range_mut(..=global_offset)
+            .range(..=global_offset)
             .last()
             .ok_or(Error::InvalidLocation)?;
         (*offset, file)
@@ -69,42 +128,40 @@ fn find_file_and_offset<F>(
     Ok((start_offset, file, next_start_offset))
 }
 
-fn write_block_to<F: io::Seek + io::Write>(
-    dest: &mut BTreeMap<usize, F>,
+fn write_block_to<F: RandomAccessReadWrite>(
+    dest: &BTreeMap<usize, F>,
     global_offset: usize,
     block: &[u8],
 ) -> Result<(), Error> {
     let (start_offset, file, next_start_offset) = find_file_and_offset(dest, global_offset)?;
-    file.seek(SeekFrom::Start((global_offset - start_offset) as u64))?;
+    let local_offset = global_offset - start_offset;
 
     let available_space = next_start_offset - global_offset;
     if block.len() <= available_space {
-        file.write_all(block)?;
-        file.flush()?;
+        file.write_all_at_offset(block, local_offset as u64)?;
         Ok(())
     } else {
         let (left, right) = block.split_at(available_space);
-        file.write_all(left)?;
-        file.flush()?;
+        file.write_all_at_offset(left, local_offset as u64)?;
         write_block_to(dest, next_start_offset, right)
     }
 }
 
-fn read_block_from<F: io::Seek + io::Read>(
-    src: &mut BTreeMap<usize, F>,
+fn read_block_from<F: RandomAccessReadWrite>(
+    src: &BTreeMap<usize, F>,
     global_offset: usize,
     dest: &mut [u8],
 ) -> Result<(), Error> {
     let (start_offset, file, next_start_offset) = find_file_and_offset(src, global_offset)?;
-    file.seek(SeekFrom::Start((global_offset - start_offset) as u64))?;
+    let local_offset = global_offset - start_offset;
 
     let available_space = next_start_offset - global_offset;
     if dest.len() <= available_space {
-        file.read_exact(dest)?;
+        file.read_all_at_offset(dest, local_offset as u64)?;
         Ok(())
     } else {
         let (left, right) = dest.split_at_mut(available_space);
-        file.read_exact(left)?;
+        file.read_all_at_offset(left, local_offset as u64)?;
         read_block_from(src, next_start_offset, right)
     }
 }
@@ -112,54 +169,67 @@ fn read_block_from<F: io::Seek + io::Read>(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::Cursor;
+    use std::cell::RefCell;
+    use std::io::{Cursor, Read, Seek, SeekFrom, Write};
+
+    impl RandomAccessReadWrite for RefCell<Cursor<Vec<u8>>> {
+        fn read_at_offset(&self, dest: &mut [u8], offset: u64) -> io::Result<usize> {
+            self.borrow_mut().seek(SeekFrom::Start(offset))?;
+            self.borrow_mut().read(dest)
+        }
+
+        fn write_at_offset(&self, src: &[u8], offset: u64) -> io::Result<usize> {
+            self.borrow_mut().seek(SeekFrom::Start(offset))?;
+            self.borrow_mut().write(src)
+        }
+    }
 
     #[test]
     fn test_write_piece_within_one_file() {
-        let mut map = BTreeMap::<usize, Cursor<Vec<u8>>>::new();
-        map.insert(0, Cursor::new(vec![0u8; 10]));
-        map.insert(10, Cursor::new(vec![0u8; 10]));
-        map.insert(20, Cursor::new(vec![0u8; 10]));
-        map.insert(30, Cursor::new(Vec::<u8>::new()));
+        let mut map = BTreeMap::<usize, RefCell<Cursor<Vec<u8>>>>::new();
+        map.insert(0, RefCell::new(Cursor::new(vec![0u8; 10])));
+        map.insert(10, RefCell::new(Cursor::new(vec![0u8; 10])));
+        map.insert(20, RefCell::new(Cursor::new(vec![0u8; 10])));
+        map.insert(30, RefCell::new(Cursor::new(Vec::<u8>::new())));
 
         write_block_to(&mut map, 16, vec![1u8, 2u8, 3u8, 4u8].as_ref()).unwrap();
 
-        assert_eq!(&vec![0u8; 10], map.get(&0).unwrap().get_ref());
+        assert_eq!(&vec![0u8; 10], map.get(&0).unwrap().borrow().get_ref());
         assert_eq!(
             &vec![0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 1u8, 2u8, 3u8, 4u8],
-            map.get(&10).unwrap().get_ref()
+            map.get(&10).unwrap().borrow().get_ref()
         );
-        assert_eq!(&vec![0u8; 10], map.get(&20).unwrap().get_ref());
+        assert_eq!(&vec![0u8; 10], map.get(&20).unwrap().borrow().get_ref());
     }
 
     #[test]
     fn test_write_piece_on_file_boundary() {
-        let mut map = BTreeMap::<usize, Cursor<Vec<u8>>>::new();
-        map.insert(0, Cursor::new(vec![0u8; 10]));
-        map.insert(10, Cursor::new(vec![0u8; 10]));
-        map.insert(20, Cursor::new(vec![0u8; 10]));
-        map.insert(30, Cursor::new(Vec::<u8>::new()));
+        let mut map = BTreeMap::<usize, RefCell<Cursor<Vec<u8>>>>::new();
+        map.insert(0, RefCell::new(Cursor::new(vec![0u8; 10])));
+        map.insert(10, RefCell::new(Cursor::new(vec![0u8; 10])));
+        map.insert(20, RefCell::new(Cursor::new(vec![0u8; 10])));
+        map.insert(30, RefCell::new(Cursor::new(Vec::<u8>::new())));
 
         write_block_to(&mut map, 17, vec![1u8, 2u8, 3u8, 4u8, 5u8].as_ref()).unwrap();
 
-        assert_eq!(&vec![0u8; 10], map.get(&0).unwrap().get_ref());
+        assert_eq!(&vec![0u8; 10], map.get(&0).unwrap().borrow().get_ref());
         assert_eq!(
             &vec![0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 1u8, 2u8, 3u8],
-            map.get(&10).unwrap().get_ref()
+            map.get(&10).unwrap().borrow().get_ref()
         );
         assert_eq!(
             &vec![4u8, 5u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8],
-            map.get(&20).unwrap().get_ref()
+            map.get(&20).unwrap().borrow().get_ref()
         );
     }
 
     #[test]
     fn test_read_piece_within_one_file() {
-        let mut map = BTreeMap::<usize, Cursor<Vec<u8>>>::new();
-        map.insert(0, Cursor::new((0u8..10u8).collect()));
-        map.insert(10, Cursor::new((0u8..10u8).collect()));
-        map.insert(20, Cursor::new((0u8..10u8).collect()));
-        map.insert(30, Cursor::new(Vec::<u8>::new()));
+        let mut map = BTreeMap::<usize, RefCell<Cursor<Vec<u8>>>>::new();
+        map.insert(0, RefCell::new(Cursor::new((0u8..10u8).collect())));
+        map.insert(10, RefCell::new(Cursor::new((0u8..10u8).collect())));
+        map.insert(20, RefCell::new(Cursor::new((0u8..10u8).collect())));
+        map.insert(30, RefCell::new(Cursor::new(Vec::<u8>::new())));
 
         let mut dest = vec![0u8; 3];
         read_block_from(&mut map, 12, &mut dest).unwrap();
@@ -168,24 +238,24 @@ mod tests {
 
         assert_eq!(
             &(0u8..10u8).collect::<Vec<u8>>(),
-            map.get(&0).unwrap().get_ref()
+            map.get(&0).unwrap().borrow().get_ref()
         );
         assert_eq!(
             &(0u8..10u8).collect::<Vec<u8>>(),
-            map.get(&10).unwrap().get_ref()
+            map.get(&10).unwrap().borrow().get_ref()
         );
         assert_eq!(
             &(0u8..10u8).collect::<Vec<u8>>(),
-            map.get(&20).unwrap().get_ref()
+            map.get(&20).unwrap().borrow().get_ref()
         );
     }
 
     #[test]
     fn test_read_piece_on_file_boundary() {
-        let mut map = BTreeMap::<usize, Cursor<Vec<u8>>>::new();
-        map.insert(0, Cursor::new((1u8..=10u8).collect()));
-        map.insert(10, Cursor::new((1u8..=10u8).collect()));
-        map.insert(20, Cursor::new(Vec::<u8>::new()));
+        let mut map = BTreeMap::<usize, RefCell<Cursor<Vec<u8>>>>::new();
+        map.insert(0, RefCell::new(Cursor::new((1u8..=10u8).collect())));
+        map.insert(10, RefCell::new(Cursor::new((1u8..=10u8).collect())));
+        map.insert(20, RefCell::new(Cursor::new(Vec::<u8>::new())));
 
         let mut dest = vec![0u8; 3];
         read_block_from(&mut map, 8, &mut dest).unwrap();
@@ -194,11 +264,11 @@ mod tests {
 
         assert_eq!(
             &(1u8..=10u8).collect::<Vec<u8>>(),
-            map.get(&0).unwrap().get_ref()
+            map.get(&0).unwrap().borrow().get_ref()
         );
         assert_eq!(
             &(1u8..=10u8).collect::<Vec<u8>>(),
-            map.get(&10).unwrap().get_ref()
-        )
+            map.get(&10).unwrap().borrow().get_ref()
+        );
     }
 }
