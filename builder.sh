b5f076620e7e311e93693423355cf30088adf9b1
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2024-03-23 16:07:30 +0000

    Move Dockerfile to .devcontainer/ folder

--- builder.sh
+++ builder.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t mtorrent_builder . && \
+docker build -t mtorrent_builder .devcontainer/ && \
 docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/mtorrent -w /usr/src/mtorrent mtorrent_builder "$@"
--- devshell.sh
+++ devshell.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILDKIT=0 docker build -t mtorrent_builder . && \
+DOCKER_BUILDKIT=0 docker build -t mtorrent_builder .devcontainer/ && \
 docker run -it --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/mtorrent -w /usr/src/mtorrent --entrypoint /bin/bash mtorrent_builder
