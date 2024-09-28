f65569f8d0fa1cb8f3262d5469c8f467cbdaa403
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2024-09-28 18:41:57 +0100

    dht::queries: use local channels

--- src/utils/local_sync/mod.rs
+++ src/utils/local_sync/mod.rs
@@ -1,9 +1,11 @@
 pub mod channel;
 pub mod condvar;
+pub mod oneshot;
 pub mod semaphore;
 mod shared_state;
 
 pub use channel::channel;
 pub use condvar::condvar;
+pub use oneshot::oneshot;
 #[allow(unused_imports)]
 pub use semaphore::semaphore;
