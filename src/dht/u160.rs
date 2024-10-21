43d6feb3e696ba66bf5882a87b3dba5594be9872
DanglingPointer <mikail.vasilyev@gmail.com>
2024-10-21 16:58:34 +0000

    u160: bitwise operations

--- src/dht/u160.rs
+++ src/dht/u160.rs
@@ -1,8 +1,9 @@
 use super::error::Error;
 use crate::utils::benc;
+use bitvec::prelude::*;
 use std::{fmt, iter, ops};
 
-/// 160-bit unsigned integer in big endian that can be XOR'ed and compared.
+/// 160-bit unsigned integer in big endian that can be XOR'ed, compared and bit-shifted.
 #[derive(PartialEq, PartialOrd, Eq, Ord)]
 pub struct U160([u8; 20]);
 
@@ -65,6 +66,89 @@ impl ops::BitXorAssign for U160 {
     }
 }
 
+impl ops::ShrAssign<usize> for U160 {
+    fn shr_assign(&mut self, rhs: usize) {
+        self.0.as_mut_bits::<Msb0>().shift_right(rhs);
+    }
+}
+
+impl ops::Shr<usize> for U160 {
+    type Output = Self;
+
+    fn shr(mut self, rhs: usize) -> Self::Output {
+        self >>= rhs;
+        self
+    }
+}
+
+impl ops::ShlAssign<usize> for U160 {
+    fn shl_assign(&mut self, rhs: usize) {
+        self.0.as_mut_bits::<Msb0>().shift_left(rhs);
+    }
+}
+
+impl ops::Shl<usize> for U160 {
+    type Output = Self;
+
+    fn shl(mut self, rhs: usize) -> Self::Output {
+        self <<= rhs;
+        self
+    }
+}
+
+impl ops::Not for U160 {
+    type Output = Self;
+
+    fn not(mut self) -> Self::Output {
+        for byte in &mut self.0 {
+            *byte = !*byte;
+        }
+        U160(self.0)
+    }
+}
+
+impl ops::AddAssign for U160 {
+    fn add_assign(&mut self, rhs: Self) {
+        let mut carry = false;
+        for (left, right) in iter::zip(&mut self.0, rhs.0).rev() {
+            let (sum, new_carry) = left.overflowing_add(right);
+            *left = sum.wrapping_add(carry as u8);
+            carry = new_carry;
+        }
+    }
+}
+
+impl ops::Add for U160 {
+    type Output = Self;
+
+    fn add(mut self, rhs: Self) -> Self::Output {
+        self += rhs;
+        self
+    }
+}
+
+impl ops::SubAssign for U160 {
+    fn sub_assign(&mut self, rhs: Self) {
+        use std::ops::AddAssign;
+        const ONE: U160 = const {
+            let mut one = [0u8; 20];
+            one[19] = 1;
+            U160(one)
+        };
+        self.add_assign(!rhs);
+        self.add_assign(ONE);
+    }
+}
+
+impl ops::Sub for U160 {
+    type Output = Self;
+
+    fn sub(mut self, rhs: Self) -> Self::Output {
+        self -= rhs;
+        self
+    }
+}
+
 impl fmt::Debug for U160 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for byte in self.0 {
@@ -105,5 +189,61 @@ mod tests {
         a ^= [0b01010101; 20].into();
 
         assert_eq!(a, [0xff; 20].into());
+    }
+
+    #[test]
+    fn test_shr_u160() {
+        let a: U160 = [0b00100001; 20].into();
+        let mut expected = [0b10010000; 20];
+        expected[0] = 0b00010000;
+        assert_eq!(a >> 1, expected.into());
+    }
+
+    #[test]
+    fn test_shl_u160() {
+        let a: U160 = [0b10000100; 20].into();
+        let mut expected = [0b00001001; 20];
+        expected[19] = 0b00001000;
+        assert_eq!(a << 1, expected.into());
+    }
+
+    #[test]
+    fn test_not_u160() {
+        let a: U160 = [0b10101010; 20].into();
+        assert_eq!(!a, [0b01010101; 20].into());
+    }
+
+    #[test]
+    fn test_add_u160() {
+        let a: U160 = [0xff; 20].into();
+        let b: U160 = [0x01; 20].into();
+        let mut expected = [0x01; 20];
+        expected[19] = 0x00;
+
+        assert_eq!(a + b, expected.into());
+    }
+
+    #[test]
+    fn test_sub_u160() {
+        let a: U160 = [0xff; 20].into();
+        let b: U160 = [0x01; 20].into();
+
+        assert_eq!(a - b, [0xfe; 20].into());
+    }
+
+    #[test]
+    fn test_sub_u160_with_borrow() {
+        let mut a = [0x00; 20];
+        a[0] = 0x01;
+        let a: U160 = a.into();
+
+        let mut b = [0x00; 20];
+        b[19] = 0x01;
+        let b: U160 = b.into();
+
+        let mut expected = [0xff; 20];
+        expected[0] = 0x00;
+
+        assert_eq!(a - b, expected.into());
     }
 }
