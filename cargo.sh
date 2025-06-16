3bb41ac7359bff4e502bf938219c7509d18859c8
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2025-06-16 18:00:21 +0100

    devcontainer: add tools for test coverage

--- cargo.sh
+++ cargo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t mtorrent_builder .devcontainer/ && \
-docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/mtorrent -w /usr/src/mtorrent mtorrent_builder "$@"
+docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/mtorrent -w /usr/src/mtorrent --entrypoint /usr/local/cargo/bin/cargo mtorrent_builder "$@"
