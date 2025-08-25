df44575a554e3b134bc350cb1366e0a9c9a0e8a9
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2025-08-25 13:27:57 +0100

    Rename ops::tracker -> ops::announces

--- mtorrent/src/ops/mod.rs
+++ mtorrent/src/ops/mod.rs
@@ -1,11 +1,12 @@
 #[macro_use]
 mod ctx;
+mod announces;
 mod connections;
 mod ctrl;
 mod peer;
 mod search;
-mod tracker;
 
+pub(crate) use announces::{make_periodic_announces, make_preliminary_announces};
 pub(crate) use connections::*;
 pub(crate) use ctx::{Handle, MainCtx, PreliminaryCtx};
 pub(crate) use ctx::{periodic_metadata_check, periodic_state_dump};
@@ -15,4 +16,3 @@ pub(crate) use peer::{
     run_pwp_listener,
 };
 pub(crate) use search::run_dht_search;
-pub(crate) use tracker::{make_periodic_announces, make_preliminary_announces};
